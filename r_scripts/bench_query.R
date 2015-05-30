8772860385b9663ec55f335f6781c27fe5f5a7c1
Damien Dosimont <damien.dosimont@imag.fr>
2015-05-30 13:50:06 +0200

    catia

--- r_scripts/bench_query.R
+++ r_scripts/bench_query.R
@@ -5,7 +5,7 @@ Sys.setlocale("LC_MESSAGES", 'french')
 bench <- "benchmark_query/20150105.csv"
 
 h <- 4
-w <- 8
+w <- 10
 
 read_data <- function(file) {
   df <- read.csv(file, header=TRUE, sep = ";", strip.white=TRUE)
@@ -17,22 +17,21 @@ read_data <- function(file) {
 printQuery <- function(data){
 dtemp<-data
 dtemp<-dtemp[(dtemp$TS %in% 1000),]
-xlabel<- "Nombre d'évènements de la trace"
-ylabel<- "Temps d'exécution (s)"
-legend<- "Temps de la requête et de construction du modèle microsopique"
+xlabel<- "Trace event number"
+ylabel<- "Execution time (s)"
 plot<-ggplot(dtemp, aes(x=EVENTS, y=TOTAL_TIME/1000, color=QUERY_TYPE, 
 shape=QUERY_TYPE))
 plot<-plot + geom_point()
 plot<-plot + geom_line()
 #plot<-plot + theme_bw()
 plot<-plot + labs(x=xlabel,y=ylabel)
-plot<-plot + scale_color_discrete(name="Optimisation de la requête",
-breaks=c("OPT", "NOPT", "T", "EP", "ET", "TEP", "TET", "EPET"),
-labels=c("T+S+J", "Pas d'Optimisation", "T", "S", "J", "T+S", "T+J", "S+J" ))
-plot<-plot+ scale_shape_manual(name="Optimisation de la requête",
+plot<-plot + scale_color_discrete(name="Query Optimization",
+breaks=c("NOPT", "T", "ET", "TET" "EP", "TEP", "EPET", "OPT"),
+labels=c("No Optmization", "Time", "Event Types", "Time + Event Types", "Space", "Time + Space", "Space + Event Types", "Full Optimization" ))
+plot<-plot+ scale_shape_manual(name="Query Optimization",
 values=c(0,1,2,3,4,5,6,7),
-breaks=c("OPT", "NOPT", "T", "EP", "ET", "TEP", "TET", "EPET"),
-labels=c("T+S+J", "Pas d'Optimisation", "T", "S", "J", "T+S", "T+J", "S+J" ))
+breaks=c("NOPT", "T", "ET", "TET" "EP", "TEP", "EPET", "OPT"),
+labels=c("No Optmization", "Time", "Event Types", "Time + Event Types", "Space", "Time + Space", "Space + Event Types", "Full Optimization" ))
 plot
 }
 
